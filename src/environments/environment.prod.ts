20a547f620bd92b815362b1bb367c7566f2a6946
Denis Tagaev <sivean.27@gmail.com>
2023-03-08 18:34:20 -0500

    env adjusted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
-  apiKey: "AIzaSyCA4fq90zlLsWzg_YLFsfCJBOkeWy2Cgao",
-  authDomain: "smsys-7e1ba.firebaseapp.com",
-  projectId: "smsys-7e1ba",
-  storageBucket: "smsys-7e1ba.appspot.com",
-  messagingSenderId: "1097110448304",
-  appId: "1:1097110448304:web:9e131dd5329760bee6b375"
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCA4fq90zlLsWzg_YLFsfCJBOkeWy2Cgao",
+    authDomain: "smsys-7e1ba.firebaseapp.com",
+    projectId: "smsys-7e1ba",
+    storageBucket: "smsys-7e1ba.appspot.com",
+    messagingSenderId: "1097110448304",
+    appId: "1:1097110448304:web:9e131dd5329760bee6b375"
+  }
 };
